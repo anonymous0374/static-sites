44b2f11aaddca8e47c27fc65cf458c152af37c6a
anonymous0374 <377738226@qq.com>
2017-09-12 18:57:04 +0800

    get data from backend

--- src/public/sub-pages/tongli-logistic-data2/javascripts/main.js
+++ src/public/sub-pages/tongli-logistic-data2/javascripts/main.js
@@ -4,30 +4,64 @@
         return;
     }
 
-    // config of all charts on this page
+    // get basic configs of all charts on this page
     var config = getConfigTemplate();
 
+    $(function() { // refresh the charts        
+        generateCharts();
+        setInterval(generateCharts, 3000);
 
-    $(function() {
+    });
+
+    // fetch data and initialize charts after DOM is ready for manipulation
+    function generateCharts() {
         var charts = []; // eChart instances
 
-        // fetch data and initialize charts after DOM is ready for manipulation
         $(Object.keys(config)).each(function(index, key) {
             (function() {
                 var chart = {};
+                var promise = $.ajax({
+                    url: config[key].url,
+                });
+
                 chart = echarts.init($('#' + config[key].elId)[0]);
+                chart.clear();
                 charts.push({
                     'chart': chart,
+                    'type': config[key].type,
                     'id': config[key].elId
                 });
 
-                // fetch data asynchronously
-                // var promise = new Promise();
-                chart.setOption(config[key]);
-            }());
-        });
+                promise.then(function(rsp) {
+                    return dataResolved(rsp, config[key].type);
+                }).then(function(data) {
+                    var option = config[key];
 
-    });
+                    // ALERT!!!! Code to format two special bar charts
+                    if (["empty-top5", "loaded-top5"].indexOf(option.elId) !== -1) {
+                        var updatedData =
+                            data.yAxis.data.map(function(item) {
+                                return {
+                                    value: item,
+                                    textStyle: {
+                                        fontSize: 10,
+                                        color: 'white',
+                                        padding: [-40, -70, 0, 0]
+                                    }
+                                };
+                            });
+                        data.series.data = data.series.data.length > 5 ? data.series.data.slice(0, 5) : data.series.data;
+                        data.yAxis.data = updatedData.length >= 5 ? updatedData.slice(0, 5) : updatedData;
+                    }
+                    // END ALERT!!!! Code to format two special bar charts
+
+                    chart.setOption(option, false, true);
+                    chart.setOption(data, false, true);
+                });
+
+            }());
+        })
+    }
 
     /////////////////////  CONFIGURATIONS BELOW //////////////////////////////////////////////
 
@@ -36,6 +70,7 @@
         // basic settings of bar-charts
         var barOptBase = {
             elId: 'leng-lian-frequent',
+            type: 'bar',
             color: ['#3398DB'],
             tooltip: {
                 trigger: 'axis',
@@ -52,7 +87,7 @@
             yAxis: [{
                 type: 'category',
                 showTitle: false,
-                data: ['广州', '北京', '南京', '上海', '武汉', '西双版纳', '玉溪', '郑州', '南宁', '杭州'],
+                data: [],
                 axisTick: {
                     show: false
                 },
@@ -79,7 +114,7 @@
                 itemStyle: {
                     barBorderRadius: 40
                 },
-                data: [10, 50, 100, 150, 200, 250, 300, 350, 400, 450]
+                data: []
             }]
 
         };
@@ -88,9 +123,9 @@
         var barOptBase2 = $.extend(true, {}, barOptBase, {
             elId: 'full-top5',
             grid: {
-                left: '-15%',
-                top: '5%',
-                bottom: '-5%',
+                left: '-10%',
+                top: '10%',
+                //bottom: '-5%',
                 containLabel: true
             },
             yAxis: {
@@ -109,7 +144,7 @@
                     textStyle: {
                         fontSize: 10,
                         color: 'white',
-                        padding: [-40, -90, 0, 0]
+                        padding: [0, 0, 0, 0]
                     }
                 }, {
                     value: 'No.4. 玉溪 -- 贵阳',
@@ -151,10 +186,7 @@
         // basic settings of line-charts
         var lineOptBase = {
             color: ['#3398DB'],
-            tooltip: {
-                trigger: 'item',
-                formatter: '{a} <br/>{b} : {c}'
-            },
+            type: 'line',
             xAxis: {
                 type: 'category',
                 axisLine: {
@@ -196,7 +228,6 @@
                 splitLine: {
                     show: false
                 },
-                // name: 'y',
                 axisTick: {
                     show: false
                 }
@@ -208,14 +239,9 @@
                 containLabel: true
             },
             series: [{
-                // name: '3的指数',
                 type: 'line',
-                lineStyle: {
-                    // normal:
-                },
                 data: [10, 90, 70, 80, 90, 100, 20, 50, 40, 30, 60, 30]
             }, {
-                // name: '2的指数',
                 type: 'line',
                 lineStyle: {
                     normal: {
@@ -232,12 +258,14 @@
                     }
                 },
                 data: [80, 90, 170, 180, 150, 140, 130, 160, 130, 190, 200, 120]
-            }]
+            }],
+            url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=4"
         };
 
         // basic settings of a pie chart
         var pieOptBase = {
             elId: 'chechang',
+            type: 'pie',
             series: [{
                 hoverAnimation: true,
                 radius: [60, 80],
@@ -258,7 +286,6 @@
                     normal: {
                         lineStyle: {
                             color: '#999',
-
                         }
                     }
                 },
@@ -280,34 +307,195 @@
                     color: '#00abf3;'
                 }, {
                     value: 5,
-                    name: '2米一下',
+                    name: '2米以下',
                     color: '#00abf3;'
                 }, ]
-            }]
+            }],
+            url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=5"
         };
 
         // actual configurations of all charts on the page
         var config = {
-            option_lenglian_frequent_bar: $.extend(true, {}, barOptBase),
+            option_lenglian_frequent_bar: $.extend(true, {}, barOptBase, {
+                url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=1"
+            }),
             option_lingdan_frequent_bar: $.extend(true, {}, barOptBase, {
-                elId: 'lingdan-frequent'
+                elId: 'lingdan-frequent',
+                url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=2"
             }),
             option_juanyan_frequent_bar: $.extend(true, {}, barOptBase, {
-                elId: 'juanyan-frequent'
+                elId: 'juanyan-frequent',
+                url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=3"
             }),
             option_order_trends_line: $.extend(true, {}, lineOptBase, {
-                elId: 'order-trends'
+                elId: 'order-trends',
+                url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=4"
             }),
             option_chechang_pie: pieOptBase,
             option_empty_top5: $.extend(true, {}, barOptBase2, {
-                elId: 'empty-top5'
+                elId: 'empty-top5',
+                url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=6"
             }),
             option_loaded_top5: $.extend(true, {}, barOptBase2, {
-                elId: 'loaded-top5'
+                elId: 'loaded-top5',
+                url: "http://statictest.tf56.com/bigDataBigScreenWeb/boarddatayunan/getYuXiLoadPortLogisticData?type=7"
             })
         };
 
         return config;
+    }
+
+    // data promise status-change event handlers
+    function barDataResolved(rsp) {
+        var response = [];
+        var option = {}; // option relayed upon response
+        var series = [];
+        var yAxis = [];
+
+        try {
+            response = JSON.parse(rsp).data;
+            $(response).each(function(index, item) {
+                series.push(item.valueLong);
+                yAxis.push(item.valueText);
+            });
+
+            option = {
+                yAxis: {
+                    data: yAxis
+                },
+                series: {
+                    data: series
+                }
+            };
+        } catch (e) {
+
+        }
+
+
+        return option;
+    };
+
+    function barDataFailed(e) {
+        console.log(e);
+    }
+
+    function pieDataResolved(rsp) {
+        var response = [];
+        var option = {}; // option relayed upon response
+        var series = [];
+
+        try {
+            response = JSON.parse(rsp).data;
+            $(response).each(function(index, item) {
+                series.push({
+                    name: item.valueText,
+                    value: item.valueLong
+                });
+            });
+
+            option = {
+                series: {
+                    data: series
+                }
+            };
+        } catch (e) {
+
+        }
+
+
+        return option;
+    }
+
+    function pieDataFailed(e) {
+
+    }
+
+    function lineDataResolved(rsp) {
+        var response = [];
+        var option = {}; // option relayed upon response
+        var series = [];
+        var yAxis = [];
+
+        try {
+            response = JSON.parse(rsp).data;
+            $(Object.keys(response)).each(function(index, key) {
+                var series_i = response[key].sort(function(item1, item2) {
+                    var num1 = parseInt(item1.valueText),
+                        num2 = parseInt(item2.valueText);
+                    return num1 - num2;
+                });
+
+
+                // 12 months as xAxis
+                $([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).each(function(index, item) {
+                    if (item) {
+
+                    }
+                });
+                $(series_i).each(function(index, item) {
+                    if (index + 1 !== parseInt(item.valueText)) {
+                        series_i.splice(index, 0, null);
+                    }
+                });
+
+                series.push({
+                    type: 'line',
+                    data: series_i
+                });
+
+
+
+            });
+
+            function findValueOnXAxis(xAxis, objs) {
+                var i = 0;
+                var found = false;
+
+                for (i = 0; i < objs.length; i++) {
+                    if (xAxis === parseInt(objs[i].valueText)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                return found;
+            }
+        } catch (e) {
+            console.log('exception occurred!');
+        }
+
+        option = {
+            series: series
+        };
+
+        return option;
+    }
+
+    function lineDataFailed(e) {
+
+    }
+
+
+    function dataResolved(rsp, chartType) {
+        switch (chartType) {
+            case 'bar':
+                {
+                    return barDataResolved(rsp);
+                }
+            case 'line':
+                {
+                    return lineDataResolved(rsp);
+                }
+            case 'pie':
+                {
+                    return pieDataResolved(rsp);
+                }
+            default:
+                {
+                    return {};
+                }
+        }
+
     }
 
 }(jQuery, window, echarts))
